cbc19a503b622cf9d1ca646badb5e6c4ac6f3004
Andrew Owens <andrrowens@gmail.com>
2023-01-12 11:47:15 -0700

    commit tree d5b2c31d3e67e048b26238381d8f3ebc5a85f823

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+require "pry"
+
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.artists
+        unique_array = @@artists.uniq
+        unique_array
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+
+    def self.count
+        @@count
+    end
+end
+
+hit_me = Song.new('hit me baby one more time', 'Brittany Spears', 'pop')
+hit_me.artist
+Song.count
+# binding.pry
